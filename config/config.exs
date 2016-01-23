406c5807f1199d6dfc8f26cc54581e1a0e5049f5
unknown <unknown@unknown.com>
2016-01-23 22:47:12 +0100

    Config api key as env var

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,9 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+config :fulcrum,
+  endpoint: "https://api.fulcrumapp.com/api/v2",
+  api_key: System.get_env("FULCRUM_API_KEY")
+
 import_config "#{Mix.env}.exs"
 
-import_config "#{Mix.env}.secret.exs"
--- config/dev.exs
+++ config/dev.exs
@@ -1,16 +1,4 @@
 use Mix.Config
 
-config :fulcrum,
-  endpoint: "https://api.fulcrumapp.com/api/v2"
-
-# your dev.secret.exs should look something like:
-# use Mix.Config
-# config :fulcrum,
-#   api_key: "<the api key you generated on the fulcrum site>"
-
-# ...or (e.g. for Heroku)
-# config :fulcrum,
-#   api_key: System.get_env("FULCRUM_KEY")
-
 # Print only warnings and errors during test
 config :logger, level: :warn
